59f88e5d6b6d86d2afc1db4c11bb546d33215fc8
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-05-12 14:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
  const lastRow = document.createElement('tr');
  const prices = document.querySelectorAll('[data-ns-test=price]');
  let sum = 0;
+const value = Array.from(prices).map(item => Number(item.textContent));
  for(let i = 0; i < prices.length; i++) {
-  sum += parseInt(prices[i].textContent);
+  sum += value[i];
  }
  const child = document.createElement("td");
  child.setAttribute('data-ns-test', 'grandTotal');
