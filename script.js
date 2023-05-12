2f6a3145b5d16b5e59770c080c3cdbc106e710e0
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-05-12 23:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+// Get all the price elements from the table
+const prices = document.querySelectorAll('[data-ns-test="price"]');
 
- const table = document.querySelector('table');
- const lastRow = document.createElement('tr');
- const prices = document.querySelectorAll('[data-ns-test=price]');
- let sum = 0;
-const value = Array.from(prices).map(item => Number(item.textContent));
- for(let i = 0; i < prices.length; i++) {
-  sum += value[i];
- }
- const child = document.createElement("td");
- child.setAttribute('data-ns-test', 'grandTotal');
+// Calculate the total price
+let totalPrice = 0;
+prices.forEach((priceElement) => {
+  totalPrice += parseInt(priceElement.textContent);
+});
 
+// Create a new row for the total price
+const table = document.querySelector('table');
+const newRow = document.createElement('tr');
+const newCell = document.createElement('td');
 
- child.textContent = sum;
- lastRow.appendChild(child);
- table.appendChild(lastRow);
+// Set the data attribute for the total price cell
+newCell.setAttribute('data-ns-test', 'grandTotal');
+
+// Set the text content of the total price cell
+newCell.textContent = totalPrice;
+
+// Append the total price cell to the new row
+newRow.appendChild(newCell);
+
+// Append the new row to the table
+table.appendChild(newRow);
